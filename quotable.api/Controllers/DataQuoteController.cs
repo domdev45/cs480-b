6811cfc48acb02cca1b90ce4d6890932afcc2d7f
domdev45 <54961914+domdev45@users.noreply.github.com>
2019-11-21 17:19:13 -0500

    Add files via upload

---
+++ quotable.api/Controllers/DataQuoteController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using quotable.core;
+using quotable.core.data;
+using quote = quotable.api.models.quote;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace quotable.api.Controllers
+{
+    /// <summary>
+	/// API controller for the '/documents' resource.
+	/// </summary>
+	[Route("api/[controller]")]
+    [ApiController]
+    public class DataQuoteController : ControllerBase
+    {
+        private readonly quoteableDbContext _context;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">The database context to data access.</param>
+        public DataQuoteController(quoteableDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        /// <summary>
+        /// Returns all the documents.
+        /// </summary>
+        /// <returns>All the documents.</returns>
+        [HttpGet]
+        public IEnumerable<quote> Get()
+        {
+            return from document in _context.Quotes
+                   select new quote()
+                   {
+                       Quote = document.Quotes
+                   };
+        }
+
+        // GET api/values/5
+        /// <summary>
+        /// Gets a specific document.
+        /// </summary>
+        /// <param name="id">The id of the document to get.</param>
+        /// <returns>The document.</returns>
+        [HttpGet("{id}")]
+        public ActionResult<quote> Get(long id)
+        {
+            var document = _context.Quotes.SingleOrDefault(d => d.Id == id);
+
+            if (document == null)
+            {
+                return NotFound();
+            }
+
+            return new quote()
+            {
+                Quote = document.Quotes
+            };
+        }
+    }
+
+}
--- quotable.api/Startup.cs
+++ quotable.api/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using quotable.core;
+using quotable.core.data;
 
 namespace quotable.api
 {
@@ -25,7 +28,8 @@ namespace quotable.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddDbContext<quoteableDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +46,54 @@ namespace quotable.api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<quoteableDbContext>();
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                PopulateDatabase(context);
+            }
+        }
+
+        private static void PopulateDatabase(quoteableDbContext context)
+        {
+            var author1 = new Author()
+            {
+                FirstName = "Dr",
+                LastName = "Seuss"
+            };
+            var author2 = new Author()
+            {
+                FirstName = "Mr",
+                LastName = "Spock"
+            };
+            var author3 = new Author()
+            {
+                FirstName = "Mr",
+                LastName = "Tuvok"
+            };
+
+            var document1 = new quote();
+            document1.Quotes = "Green Eggs and Ham";
+
+            var document2 = new quote();
+            document2.Quotes = "Vulcan, A Primer";
+
+            var document3 = new quote();
+            document3.Quotes = "Green Eggs and Vulcans";
+
+            var da1 = new quoteAuthor() { Quote = document1, Author = author1 };
+            var da2 = new quoteAuthor() { Quote = document2, Author = author2 };
+            var da3 = new quoteAuthor() { Quote = document3, Author = author1 };
+            var da4 = new quoteAuthor() { Quote = document3, Author = author2 };
+            var da5 = new quoteAuthor() { Quote = document3, Author = author3 };
+
+            context.AddRange(da1, da2, da3, da4, da5);
+
+            context.SaveChanges();
         }
     }
+
 }
+
